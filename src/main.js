7a0d378115b6615e267be4b0da3afea38aa263a5
Gabriel Guzowski <guzowski@Visagio-1098.localdomain>
2020-04-05 19:34:42 -0300

    Adding Axios, Bootstrap and FireStore

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { firestorePlugin } from 'vuefire'
+
+Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
 
